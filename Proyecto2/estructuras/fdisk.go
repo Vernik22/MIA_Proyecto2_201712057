39fd36dc280e62d331ca79b40f118c660944cad2
Vernik <vernik22.ing@gmail.com>
2022-04-12 13:15:44 -0600

    fdisk terminado y creado mount

--- Proyecto2/estructuras/fdisk.go
+++ Proyecto2/estructuras/fdisk.go
@@ -37,13 +37,57 @@ func EjecutarComandoFdisk(datFormat PropFdisk) {
 				fmt.Println("No existe el archivo en la ruta")
 			} else {
 				if string(mbrTemp.Mbr_partition_1.Part_status[:]) == "0" || string(mbrTemp.Mbr_partition_2.Part_status[:]) == "0" || string(mbrTemp.Mbr_partition_3.Part_status[:]) == "0" || string(mbrTemp.Mbr_partition_4.Part_status[:]) == "0" {
-					crearParticionPrimaria(datFormat, tamanoParticion)
+					if hayEspacio(tamanoParticion, mbrTemp.Mbr_tamano) {
+						crearParticionPrimaria(datFormat, tamanoParticion)
+					} else {
+						fmt.Println("ERROR: No hay espacio suficiente en el disco")
+					}
+				} else {
+					fmt.Println("ERROR: No se pueden crear mas particiones primarias")
 				}
 			}
 
 		} else if datFormat.setType == "e" {
+			f, err := os.OpenFile(datFormat.setPath, os.O_RDWR, 0755)
+			f.Seek(0, 0)
+			err = binary.Read(f, binary.BigEndian, &mbrTemp)
+			defer f.Close()
+			if err != nil {
+				fmt.Println("No existe el archivo en la ruta")
+			} else {
+				if string(mbrTemp.Mbr_partition_1.Part_status[:]) == "0" || string(mbrTemp.Mbr_partition_2.Part_status[:]) == "0" || string(mbrTemp.Mbr_partition_3.Part_status[:]) == "0" || string(mbrTemp.Mbr_partition_4.Part_status[:]) == "0" {
+					if string(mbrTemp.Mbr_partition_1.Part_type[:]) != "e" && string(mbrTemp.Mbr_partition_2.Part_type[:]) != "e" && string(mbrTemp.Mbr_partition_3.Part_type[:]) != "e" && string(mbrTemp.Mbr_partition_4.Part_type[:]) != "e" {
+						if hayEspacio(tamanoParticion, mbrTemp.Mbr_tamano) {
+							crearParticionExtendida(datFormat, tamanoParticion)
+						} else {
+							fmt.Println("ERROR: No hay espacio suficiente en el disco")
+						}
+					} else {
+						fmt.Println("ERROR: Ya existe una particion extendida ")
+					}
+				} else {
+					fmt.Println("ERROR: No se pueden crear mas particiones ")
+				}
+			}
 
 		} else if datFormat.setType == "l" {
+			f, err := os.OpenFile(datFormat.setPath, os.O_RDWR, 0755)
+			f.Seek(0, 0)
+			err = binary.Read(f, binary.BigEndian, &mbrTemp)
+			defer f.Close()
+			if err != nil {
+				fmt.Println("No existe el archivo en la ruta")
+			} else {
+				if string(mbrTemp.Mbr_partition_1.Part_type[:]) == "e" || string(mbrTemp.Mbr_partition_2.Part_type[:]) == "e" || string(mbrTemp.Mbr_partition_3.Part_type[:]) == "e" || string(mbrTemp.Mbr_partition_4.Part_type[:]) == "e" {
+					if hayEspacio(tamanoParticion, mbrTemp.Mbr_tamano) {
+						crearParticionLogica(datFormat, tamanoParticion)
+					} else {
+						fmt.Println("ERROR: No hay espacio suficiente en el disco")
+					}
+				} else {
+					fmt.Println("ERROR: No existe una particion extendida ")
+				}
+			}
 
 		}
 
@@ -144,15 +188,249 @@ func crearParticionPrimaria(datFormat PropFdisk, tamanoPart int64) {
 			parts[1] = mbrTemp.Mbr_partition_2
 			parts[2] = mbrTemp.Mbr_partition_3
 			parts[3] = mbrTemp.Mbr_partition_4
-			if string(mbrTemp.Mbr_dsk_fit[:]) == "f" {
 
-			} else if string(mbrTemp.Mbr_dsk_fit[:]) == "w" {
+			for i := 0; i < 4; i++ {
+				if string(parts[i].Part_status[:]) == "1" {
 
-			} else if string(mbrTemp.Mbr_dsk_fit[:]) == "b" {
+					startPart = parts[i].Part_start + parts[i].Part_size
 
+				} else {
+					espL := espacioLibre(startPart, mbrTemp.Mbr_tamano)
+					if hayEspacio(tamanoPart, espL) {
+						//setenado los valores que lleva la nueva particion
+						copy(particionTemp.Part_name[:], datFormat.setName)
+						copy(particionTemp.Part_type[:], "p")
+						particionTemp.Part_size = tamanoPart
+						particionTemp.Part_start = startPart
+						copy(particionTemp.Part_status[:], "1")
+						for f := 0; f < 4; f++ {
+							if string(parts[f].Part_status[:]) == "0" {
+								parts[f] = particionTemp
+								break
+							}
+						}
+						fmt.Println("Se creo la particion : " + datFormat.setName + " Correctamente")
+						break
+
+					} else {
+						fmt.Println("ERROR: No hay espacio suficiente para crear la particion ")
+					}
+				}
 			}
+			mbrTemp.Mbr_partition_1 = parts[0]
+			mbrTemp.Mbr_partition_2 = parts[1]
+			mbrTemp.Mbr_partition_3 = parts[2]
+			mbrTemp.Mbr_partition_4 = parts[3]
+			//modificar el mbr en el disco
+			f.Seek(0, 0)
+			err = binary.Write(f, binary.BigEndian, mbrTemp)
+
+			/*
+				if string(mbrTemp.Mbr_dsk_fit[:]) == "f" {
+
+				} else if string(mbrTemp.Mbr_dsk_fit[:]) == "w" {
+
+				} else if string(mbrTemp.Mbr_dsk_fit[:]) == "b" {
+
+				}
+			*/
 		}
 
+	}
+}
+
+func crearParticionExtendida(datFormat PropFdisk, tamanoPart int64) {
+	mbrTemp := MBR{}
+	particionTemp := Particion{}
+	var startPart int64 = int64(unsafe.Sizeof(mbrTemp))
+
+	if datFormat.setFit == "bf" {
+		copy(particionTemp.Part_fit[:], "b")
+	} else if datFormat.setFit == "wf" {
+		copy(particionTemp.Part_fit[:], "w")
+	} else if datFormat.setFit == "ff" {
+		copy(particionTemp.Part_fit[:], "f")
+	}
+
+	f, err := os.OpenFile(datFormat.setPath, os.O_RDWR, 0755)
+	f.Seek(0, 0)
+	err = binary.Read(f, binary.BigEndian, &mbrTemp)
+	defer f.Close()
+	if err != nil {
+		fmt.Println("No existe el archivo en la ruta")
+	} else {
+		var parts [4]Particion
+		//seteando el ebr nuevo
+		vacia := EBR{}
+		copy(vacia.Part_fit[:], "-")
+		copy(vacia.Part_name[:], "0")
+		vacia.Part_next = -1
+		vacia.Part_size = -1
+		vacia.Part_start = -1
+		copy(vacia.Part_status[:], "0")
+		if string(mbrTemp.Mbr_partition_1.Part_status[:]) == "0" && string(mbrTemp.Mbr_partition_2.Part_status[:]) == "0" && string(mbrTemp.Mbr_partition_3.Part_status[:]) == "0" && string(mbrTemp.Mbr_partition_4.Part_status[:]) == "0" {
+
+			//seteando los valores que llevara la nueva particion
+			copy(particionTemp.Part_name[:], datFormat.setName)
+			copy(particionTemp.Part_type[:], "e")
+			particionTemp.Part_size = tamanoPart
+			particionTemp.Part_start = startPart
+			copy(particionTemp.Part_status[:], "1")
+			//copiando particion creada
+			mbrTemp.Mbr_partition_1 = particionTemp
+			//ahora modificamos el mbr original en el archivo por el nuevo mbr con los datos de la particion creada
+			f.Seek(0, 0)
+			err = binary.Write(f, binary.BigEndian, mbrTemp)
+			f.Seek(startPart, 0)
+			err = binary.Write(f, binary.BigEndian, vacia)
+			fmt.Println("Se creo la particion : " + datFormat.setName + " Correctamente")
+		} else {
+			parts[0] = mbrTemp.Mbr_partition_1
+			parts[1] = mbrTemp.Mbr_partition_2
+			parts[2] = mbrTemp.Mbr_partition_3
+			parts[3] = mbrTemp.Mbr_partition_4
+
+			for i := 0; i < 4; i++ {
+				if string(parts[i].Part_status[:]) == "1" {
+
+					startPart = parts[i].Part_start + parts[i].Part_size
+
+				} else {
+					espL := espacioLibre(startPart, mbrTemp.Mbr_tamano)
+					if hayEspacio(tamanoPart, espL) {
+						//setenado los valores que lleva la nueva particion
+						copy(particionTemp.Part_name[:], datFormat.setName)
+						copy(particionTemp.Part_type[:], "e")
+						particionTemp.Part_size = tamanoPart
+						particionTemp.Part_start = startPart
+						copy(particionTemp.Part_status[:], "1")
+						for f := 0; f < 4; f++ {
+							if string(parts[f].Part_status[:]) == "0" {
+								parts[f] = particionTemp
+								break
+							}
+						}
+						f.Seek(startPart, 0)
+						err = binary.Write(f, binary.BigEndian, vacia)
+						fmt.Println("Se creo la particion : " + datFormat.setName + " Correctamente")
+						break
+
+					} else {
+						fmt.Println("ERROR: No hay espacio suficiente para crear la particion ")
+					}
+				}
+			}
+			mbrTemp.Mbr_partition_1 = parts[0]
+			mbrTemp.Mbr_partition_2 = parts[1]
+			mbrTemp.Mbr_partition_3 = parts[2]
+			mbrTemp.Mbr_partition_4 = parts[3]
+			//modificar el mbr en el disco
+			f.Seek(0, 0)
+			err = binary.Write(f, binary.BigEndian, mbrTemp)
+		}
+
+	}
+}
+
+func crearParticionLogica(datFormat PropFdisk, tamanoPart int64) {
+	mbrTemp := MBR{}
+	ebrTemp := EBR{}
+	//var startPart int64 = int64(unsafe.Sizeof(mbrTemp))
+
+	if datFormat.setFit == "bf" {
+		copy(ebrTemp.Part_fit[:], "b")
+	} else if datFormat.setFit == "wf" {
+		copy(ebrTemp.Part_fit[:], "w")
+	} else if datFormat.setFit == "ff" {
+		copy(ebrTemp.Part_fit[:], "f")
+	}
+
+	f, err := os.OpenFile(datFormat.setPath, os.O_RDWR, 0755)
+	f.Seek(0, 0)
+	err = binary.Read(f, binary.BigEndian, &mbrTemp)
+	defer f.Close()
+	if err != nil {
+		fmt.Println("No existe el archivo en la ruta")
+	} else {
+		var parts [4]Particion
+		parts[0] = mbrTemp.Mbr_partition_1
+		parts[1] = mbrTemp.Mbr_partition_2
+		parts[2] = mbrTemp.Mbr_partition_3
+		parts[3] = mbrTemp.Mbr_partition_4
+		ebrTemp1 := EBR{}
+		ebrTemp2 := EBR{}
+		var logPartStart int64 = int64(unsafe.Sizeof(ebrTemp1))
+		var exPartStart int64 = 0
+		var tamParticionEx int64 = 0
+		for i := 0; i < 4; i++ {
+			if string(parts[i].Part_type[:]) == "e" {
+				tamParticionEx += parts[i].Part_size
+				logPartStart += parts[i].Part_start
+				exPartStart += parts[i].Part_start
+				f.Seek(parts[i].Part_start, 0)
+				err = binary.Read(f, binary.BigEndian, &ebrTemp1)
+				break
+			}
+		}
+		ebrTemp2 = ebrTemp1
+		if hayEspacio(tamanoPart, tamParticionEx) {
+			//seteando el ebr nuevo
+			vacia := EBR{}
+			copy(vacia.Part_fit[:], "-")
+			copy(vacia.Part_name[:], "0")
+			vacia.Part_next = -1
+			vacia.Part_size = -1
+			vacia.Part_start = -1
+			copy(vacia.Part_status[:], "0")
+			var tamTemp int64 = int64(unsafe.Sizeof(ebrTemp1))
+			for tamTemp < tamParticionEx {
+				if string(ebrTemp1.Part_status[:]) == "0" && ebrTemp1.Part_next == -1 {
+					ebrTemp1.Part_fit = ebrTemp.Part_fit
+					copy(ebrTemp1.Part_name[:], datFormat.setName)
+					ebrTemp1.Part_next = logPartStart + tamanoPart
+					ebrTemp1.Part_size = tamanoPart
+					ebrTemp1.Part_start = logPartStart
+					copy(ebrTemp1.Part_status[:], "1")
+					f.Seek(exPartStart, 0)
+					err = binary.Write(f, binary.BigEndian, ebrTemp1)
+					f.Seek(ebrTemp1.Part_next, 0)
+					err = binary.Write(f, binary.BigEndian, vacia)
+
+					fmt.Println("Se creo la particion : " + datFormat.setName + " Correctamente")
+					break
+				} else if string(ebrTemp1.Part_status[:]) == "0" && ebrTemp1.Part_next != -1 {
+					var espac int64 = espacioLibre(ebrTemp2.Part_start, ebrTemp1.Part_next)
+					if hayEspacio(tamanoPart, espac) {
+						ebrTemp1.Part_fit = ebrTemp.Part_fit
+						copy(ebrTemp1.Part_name[:], datFormat.setName)
+						ebrTemp1.Part_next = logPartStart + tamanoPart
+						ebrTemp1.Part_size = tamanoPart
+						ebrTemp1.Part_start = logPartStart
+						copy(ebrTemp1.Part_status[:], "1")
+						f.Seek(exPartStart, 0)
+						err = binary.Write(f, binary.BigEndian, ebrTemp1)
+						f.Seek(ebrTemp1.Part_next, 0)
+						err = binary.Write(f, binary.BigEndian, vacia)
+						fmt.Println("Se creo la particion : " + datFormat.setName + " Correctamente")
+						break
+					} else {
+						ebrTemp2 = ebrTemp1
+						f.Seek(ebrTemp1.Part_next, 0)
+						err = binary.Read(f, binary.BigEndian, &ebrTemp1)
+						logPartStart = ebrTemp2.Part_next + int64(unsafe.Sizeof(ebrTemp1))
+						exPartStart = ebrTemp2.Part_next
+					}
+				} else if string(ebrTemp1.Part_status[:]) == "1" && ebrTemp1.Part_next != -1 {
+					ebrTemp2 = ebrTemp1
+					f.Seek(ebrTemp1.Part_next, 0)
+					err = binary.Read(f, binary.BigEndian, &ebrTemp1)
+					logPartStart = ebrTemp2.Part_next + int64(unsafe.Sizeof(ebrTemp1))
+					exPartStart = ebrTemp2.Part_next
+				}
+			}
+		} else {
+			fmt.Println("ERROR: No hay espacio suficiente en la particion E para crear la particion L")
+		}
 	}
 }
 
@@ -229,4 +507,8 @@ func imprimirDatosDisco(path string) {
 		}
 		fmt.Println("")
 	}
+}
+
+func espacioLibre(inicio, fin int64) int64 {
+	return fin - inicio
 }
